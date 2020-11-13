99e081b719b4b1fdd63a231c3e32fafedc844678
Alex Laurila <s181927@student.hb.se>
2020-11-13 19:06:45 +0100

    Added SelectionSortParallell

---
+++ Sorting/SelectionSortParallell.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sorting
+{
+    public class SelectionSortParallell<T> : ISort<T>
+    {
+        public string Name { get { return "SelectionSortParallell"; } }
+
+        public void Sort(T[] inputOutput)
+        {
+            Sort(inputOutput, Comparer<T>.Default);
+        }
+
+        public void Sort(T[] inputOutput, IComparer<T> comparer)
+        {
+            int n = inputOutput.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (comparer.Compare(inputOutput[j], inputOutput[min]) < 0)
+                    {
+                        T tmp = inputOutput[j];
+                        inputOutput[j] = inputOutput[min];
+                        inputOutput[min] = tmp;
+                    }
+                }
+            }
+        }
+    }
+}
