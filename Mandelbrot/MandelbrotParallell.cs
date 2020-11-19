f0cade18e353e1728f1aff117c9693e01e56f837
Alex Laurila <s181927@student.hb.se>
2020-11-19 11:18:33 +0100

    Removed comments in MandebrotParallell

--- Mandelbrot/MandelbrotParallell.cs
+++ Mandelbrot/MandelbrotParallell.cs
@@ -21,10 +21,6 @@ namespace Mandelbrot
 
         public override void Compute()
         {
-            //Compute(new Tuple<double, double>(LowerX, UpperX),
-            //    new Tuple<double, double>(LowerY, UpperY),
-            //    Image);
-
             int widthPixels = Image.GetLength(0);
             int heightPixels = Image.GetLength(1);
             double stepx = (UpperX - LowerX) / widthPixels;
@@ -41,17 +37,6 @@ namespace Mandelbrot
                     Image[i, j] = MAX_ITERATIONS - color;
                 }
             });
-
-            //for (int i = 0; i < widthPixels; i++)
-            //{
-            //    for (int j = 0; j < heightPixels; j++)
-            //    {
-            //        double tempx = LowerX + i * stepx;
-            //        double tempy = LowerY + j * stepy;
-            //        int color = Diverge(tempx, tempy);
-            //        Image[i, j] = MAX_ITERATIONS - color;
-            //    }
-            //}
         }
     }
 }
